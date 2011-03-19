82ab4cdbc531b3c50d0112373e0f93234afd50f6
Chris S <chris.spack@gmail.com>
2011-03-19 17:44:04 -0400

    Wrote some methods, fixed bugs - should compile

--- src/NBTTree.cpp
+++ src/NBTTree.cpp
@@ -1,15 +1,45 @@
 #include "NBTTree.h"
+#include "nbt_defines.h"
+#include <deque>
 
 NBT_Tag::NBT_Tag(NBT_StringHolder *name, TagType tagType)
 {
     _name = name;
     _type = tagType;
 }
+NBT_Tag::NBT_Tag(TagType tagType)
+{
+    NBT_StringHolder blank;
+    blank.length = 0;
+    blank.value=new char[1];
+    _name = &blank;
+    _type = tagType;
+}
+
+NBT_Tag::~NBT_Tag()
+{
+    // destroy NBT_StringHolder
+    if(_name != NULL)
+    {
+        if(_name->value != NULL)
+        {
+            delete[] _name->value;
+        }
+        delete _name;
+    
+    }
+    
+    //TODO: Delete root node
+    //TODO: Delete file reference
+}
+
 
 NBT_StringHolder* NBT_Tag::GetName()
 {
     return _name;
 }
+
+
 
 
 void NBT_Tag::SetName(NBT_StringHolder *name)
@@ -28,3 +58,27 @@ void NBT_Tag::SetName(NBT_StringHolder *name)
     // set new one
     _name = name;
 };
+
+//*************************
+// TAG_Compound
+//*************************
+
+TAG_Compound::TAG_Compound() : NBT_Tag(TAGTYPE_COMPOUND)
+{
+    //_name=blank;
+    // _type = TAGTYPE_Compound;
+    // NBT_Tag(blank, TAGTYPE_COMPOUND);
+}
+
+TAG_Compound::~TAG_Compound()
+{
+    // todo: delete fields queue
+}
+
+
+void TAG_Compound::add(NBT_Tag* newItem)
+{
+    //TODO: put in middle, sort by tag
+    
+    fields.push_back(newItem);
+}
