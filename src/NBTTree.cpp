3619f9c9d858fe0f5e2a2d2fd5d976990415cef3
Chris S <chris.spack@gmail.com>
2011-03-19 23:02:41 -0400

    Updated NBT Tree

--- src/NBTTree.cpp
+++ src/NBTTree.cpp
@@ -1,6 +1,7 @@
 #include "NBTTree.h"
 #include "nbt_defines.h"
 #include <deque>
+#include <iostream>
 
 NBT_Tag::NBT_Tag(NBT_StringHolder *name, TagType tagType)
 {
@@ -11,7 +12,7 @@ NBT_Tag::NBT_Tag(TagType tagType)
 {
     NBT_StringHolder blank;
     blank.length = 0;
-    blank.value=new char[1];
+    blank.value=NULL;
     _name = &blank;
     _type = tagType;
 }
@@ -34,29 +35,33 @@ NBT_Tag::~NBT_Tag()
 }
 
 
-NBT_StringHolder* NBT_Tag::GetName()
+NBT_StringHolder* NBT_Tag::getName()
 {
     return _name;
 }
 
 
 
-
-void NBT_Tag::SetName(NBT_StringHolder *name)
+void NBT_Tag::setName(NBT_StringHolder name)
 {
-    // delete old name
-    if(_name != NULL)
-    {
-        if(_name->value != NULL)
-        {
-            delete[] _name->value;
-        }
-        delete _name;
+    _name->length = name.length;
+    _name->value = name.value;
+};
+
+void NBT_Tag::setName(NBT_StringHolder *name)
+{
     
+    
+    // delete old name
+    if(_name->value != NULL)
+    {
+        delete[] _name->value;
     }
     
+    
     // set new one
-    _name = name;
+    _name->length = name->length;
+    _name->value  = name->value;
 };
 
 //*************************
@@ -82,3 +87,159 @@ void TAG_Compound::add(NBT_Tag* newItem)
     
     fields.push_back(newItem);
 }
+
+
+NBT_BYTE * TAG_Compound::parseTag(NBT_BYTE * data)
+   {
+        cout << "I love you!" << endl;
+        cout.flush();
+        
+        NBT_BYTE * start = data;
+        
+        NBT_StringHolder* name = TAG_String::Parse(data);
+        cout << "Compound tag name: " << *name->value << endl;
+        cout << "Compound tag length: " << name->length << endl;
+        cout << "Compound tag short size: " << sizeof(NBT_SHORT) << endl;
+        cout.flush();
+        
+        // apply name
+        setName(name);
+        
+        cout << "Set name " << endl;
+        cout.flush();
+        
+        data += name->length + sizeof(NBT_SHORT);
+        cout << "Moved ahead" << endl;
+        cout.flush();
+        
+        bool doContinue = true;
+        NBT_Tag * t;
+        cout << "Gonna loop now!" << endl;
+        cout.flush();
+        while(doContinue)
+        {
+            cout << "Reading byte " << (short int)(*data) << endl;
+            switch(*data)
+            {
+                // I MADE IT! :D
+                case TAGTYPE_END:
+                    
+                    data++; // position after this end marker
+                    doContinue = false;
+                    break;
+                    
+                // I HAS A BABY!
+                case TAGTYPE_COMPOUND:
+                    t = new TAG_Compound;
+                    data = ((TAG_Compound*)t)->parseTag(data);
+                    add(t);
+                    break;
+                    
+                // Atomics
+                case TAGTYPE_SHORT:
+                    t = new TAG_Short;
+                    data = ((TAG_Short*)t)->parseTag(data);
+                    add(t);
+                    break;
+                case TAGTYPE_INT:
+                    t = new TAG_Int;
+                    data = ((TAG_Int*)t)->parseTag(data);
+                    add(t);
+                    break;
+                case TAGTYPE_LONG:
+                    t = new TAG_Long;
+                    data = ((TAG_Long*)t)->parseTag(data);
+                    add(t);
+                    break;
+                case TAGTYPE_DOUBLE:
+                    t = new TAG_Double;
+                    data = ((TAG_Double*)t)->parseTag(data);
+                    add(t);
+                    break;
+                    
+                case TAGTYPE_FLOAT:
+                    t = new TAG_Float;
+                    data = ((TAG_Float*)t)->parseTag(data);
+                    add(t);
+                    break;
+                    
+                case TAGTYPE_BYTE:
+                    t = new TAG_Byte;
+                    data = ((TAG_Byte*)t)->parseTag(data);
+                    add(t);
+                    break;
+                case TAGTYPE_STRING:
+                    t = new TAG_String;
+                    data = ((TAG_String*)t)->parseTag(data);
+                    add(t);
+                    break;
+                default:
+                    cout << "ERROR Reading - Unknown TAG Type: " << (short int)*data << endl;
+                    break;
+
+            };
+            
+
+        }
+        
+        return data;
+    }
+
+//*************************
+// TAG_String
+//*************************
+
+
+TAG_String::TAG_String() : NBT_Tag(TAGTYPE_STRING)
+{
+    return;
+};
+
+
+NBT_StringHolder* TAG_String::Parse(NBT_BYTE * data)
+{
+        // grab a short from data
+        NBT_SHORT len = TAG_Short::Parse(data);
+        data += sizeof(NBT_SHORT);
+        
+        char * name = new char[len+1];
+        memcpy(name, data, len);
+        name[len] = 0; // terminate string!
+        
+        NBT_StringHolder * v = new NBT_StringHolder;
+        v->length = len;
+        v->value = name;
+        
+        return v;
+}
+
+
+NBT_BYTE * TAG_String::parseTag(NBT_BYTE * data)
+{
+    
+    // grab name of tag
+    setName(TAG_String::Parse(data));
+    // offset past name of tag
+    data += getName()->length + sizeof(NBT_SHORT);
+    
+    // get length of string
+    payload.length = TAG_Short::Parse(data);
+    
+    // offset to beginning of string
+    data += sizeof(NBT_SHORT);
+    
+    // copy length bytes
+    char * myVal = new char[payload.length + 1];
+    memcpy(myVal, data, payload.length);
+    
+    // null terminate the string
+    myVal[payload.length] = 0;
+
+    // copy new char[] into "Payload"
+    payload.value = myVal;
+
+    // position data past the end of the string    
+    return data + payload.length;
+}
+
+
--- src/NBTTree.h
+++ src/NBTTree.h
@@ -14,12 +14,14 @@ class NBT_Tag {
     private:
         NBT_StringHolder *_name;
         TagType _type;
+        int _memSize;
     public:
         NBT_Tag(TagType tagType);
         NBT_Tag(NBT_StringHolder *name, TagType tagType);
         ~NBT_Tag();
-        NBT_StringHolder* GetName();
-        void SetName(NBT_StringHolder *name);
+        NBT_StringHolder* getName();
+        void setName(NBT_StringHolder name);
+        void setName(NBT_StringHolder *name);
         TagType getType();
         bool isType(TagType tagType2);
 };
@@ -30,13 +32,55 @@ class TAG_End : NBT_Tag
     TAG_End();
 };
 
+// NOTE: Must be above TAG_Atom because it's referenced in template
+
+class TAG_String : public NBT_Tag {
+    private:
+        NBT_StringHolder payload;    
+    public:
+        TAG_String();
+        ~TAG_String();
+        static NBT_StringHolder * Parse(NBT_BYTE * data);
+        NBT_BYTE * parseTag(NBT_BYTE * data);
+        
+};
+
 template <class E, TagType tagVal>
 class TAG_Atom : public NBT_Tag {
     private:
         E payload;
     public:
+        
+        TAG_Atom() : NBT_Tag(tagVal)
+        {
+            return;            
+        }
+        
         TagType getType() {
             return tagVal;
+        }
+        
+        NBT_BYTE * parseTag(NBT_BYTE * data)
+        {
+            // get name
+            /*
+            len t = TAG_Short::Parse(data);
+            start += sizeof(NBT_SHORT);
+            // etc etc...
+            */
+            setName(TAG_String::Parse(data));
+            data += getName()->length + 2;
+            
+            // get payload
+            payload = Parse(data);
+            data += sizeof(E);
+            return data;
+            
+        }
+        
+        static E Parse(NBT_BYTE * data)
+        {
+            return *((E*)data);
         }
         
         // 
@@ -57,13 +101,16 @@ class TAG_Atom : public NBT_Tag {
 
 // We may need a template specialization for TAG_String
 typedef TAG_Atom<NBT_BYTE, TAGTYPE_BYTE> TAG_Byte;
-typedef TAG_Atom<NBT_BYTE *, TAGTYPE_BYTE_ARRAY> TAG_Byte_Array;
 typedef TAG_Atom<NBT_DOUBLE, TAGTYPE_DOUBLE> TAG_Double;
 typedef TAG_Atom<NBT_FLOAT, TAGTYPE_FLOAT> TAG_Float;
 typedef TAG_Atom<NBT_INT, TAGTYPE_INT> TAG_Int;
 typedef TAG_Atom<NBT_LONG, TAGTYPE_LONG> TAG_Long;
 typedef TAG_Atom<NBT_SHORT, TAGTYPE_SHORT> TAG_Short;
-typedef TAG_Atom<NBT_StringHolder, TAGTYPE_STRING> TAG_String;
+
+
+
+
+typedef TAG_Atom<NBT_BYTE *, TAGTYPE_BYTE_ARRAY> TAG_Byte_Array;
 
 class TAG_List : public NBT_Tag {
     private:
@@ -86,6 +133,7 @@ class TAG_Compound : public NBT_Tag {
         ~TAG_Compound();
         NBT_Tag *operator[] (char *field);
         void add(NBT_Tag *newItem);
+        NBT_BYTE * parseTag(NBT_BYTE * data);
 };
 
 //NBT_Tag *parseNBT(byte *NBT_Data);
